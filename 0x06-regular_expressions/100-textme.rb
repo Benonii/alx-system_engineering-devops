cd0842ecac2d20421e3a82d900b4e78968a24fd2
root <besckinder@gmail.com>
2023-10-05 04:39:59 -0700

    Checker fix attempt 4

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/([^,]+),([^,]+),(.+)/).map { |m| "[#{m.join(',')}]" }.join
+
+puts ARGV[0].scan(/([^,]+),([^,]+),(.+)/).map { |match| "[#{match.join(',')}]" }.join
