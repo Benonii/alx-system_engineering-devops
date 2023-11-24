725d925de663ed62748d28ac1be71813f2f55291
root <besckinder@@gmail.com>
2023-11-24 14:39:46 -0800

    Checker fix attempt 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # This manifest installs flask from pip3
 
+package { 'python3-pip':
+  enusre => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
