5f8f7753db9ad3765fa42f24dc0a6f5f0b90ef06
root <besckinder@gmail.com>
2023-11-26 00:02:28 -0800

    Checker fix attempt 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,9 +3,9 @@
 file { 2-ssh_config:
   ensure  => file,
   content => "Host 75667-web-01\n\
-  	      HostName 34.229.186.0\n\
-  	      User ubuntu\n\
-  	      IdentityFile ~/.ssh/school\n\
-  	      PasswordAuthentication no\n",
+  	        HostName 34.229.186.0\n\
+  	        User ubuntu\n\
+  	        IdentityFile ~/.ssh/school\n\
+  	        PasswordAuthentication no\n",
   require => File['~/.ssh/school'].
 }
