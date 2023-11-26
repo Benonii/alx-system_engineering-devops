5979c2552b8859016aaa87c532448c40ec9070f3
root <besckinder@gmail.com>
2023-11-26 01:06:26 -0800

    Done it!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 # Deos the same thing in task 2 except in puppet
 
-file { '2-ssh_config':
+file { '/root/alx-system_engineering_devops/0x0B-ssh/2-ssh_config':
   ensure  => file,
   content => "Host 75667-web-01\n\
-  	        HostName 34.229.186.0\n\
-  	        User ubuntu\n\
-  	        IdentityFile ~/.ssh/school\n\
-  	        PasswordAuthentication no\n",
-  require => File['~/.ssh/school'].
+              HostName 34.229.186.0\n\
+              User ubuntu\n\
+              IdentityFile ~/.ssh/school\n\
+              PasswordAuthentication no\n",
 }
