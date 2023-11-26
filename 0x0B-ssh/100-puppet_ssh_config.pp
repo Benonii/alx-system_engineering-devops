900bab386be4cff7e69ef2d53a2c99ea9b09a672
root <besckinder@gmail.com>
2023-11-26 01:15:38 -0800

    Checker fix attempt 5

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Deos the same thing in task 2 except in puppet
 
-file { '/root/alx-system_engineering_devops/0x0B-ssh/2-ssh_config':
+file { '/root/alx-system_engineering-devops/0x0B-ssh/2-ssh_config':
   ensure  => file,
   content => "Host 75667-web-01\n\
               HostName 34.229.186.0\n\
