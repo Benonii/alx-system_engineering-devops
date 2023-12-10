f36a141c8648bca714923fe34a301dd2fa47a127
root <besckinder@gmail.com>
2023-12-09 17:56:47 -0800

    Attempt 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 
 file { '/root/.ssh/.ssh_config':
   ensure  => file,
-  content => "Host 75667-web-01\n\tHostName 54.146.81.93\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no\n",
+  content => "Host 75667-web-01\n\tHostName 100.25.109.172\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no\n",
 }
