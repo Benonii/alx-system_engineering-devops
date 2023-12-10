f4b55acdede63c474f7756061de22e5392b6836a
root <besckinder@gmail.com>
2023-12-09 19:47:56 -0800

    Moving on

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,5 +10,24 @@ class nginx {
 	  enable  => true,
 	  require => Package['nginx']
 	}
+
+	file { '/var/www/html/index.html':
+	  ensure  => 'present',
+	  content => 'Hello World!',
+	}
+
+	exec {'start_server':
+	  command => 'sudo service nginx start',
+	}
+
+	file { '/var/www/html/redirect.html':
+	  ensure  => 'present',
+	  content => 'Moved Permanently',
+	}
+
+	exec { 'modify_config':
+	  command => "sed -i '/server_name _;/a \tlocation /redirect_me {\n\troot var/www/html/;\n}\n' /etc/nginx/sites-available/default",
+	  path    => ['/bin', '/usr/bin'],
+	}
 }
 include nginx
