96c803ef547d45618f0d61524a2a46352572be10
root <besckinder@gmail.com>
2023-12-09 19:54:44 -0800

    Not just yet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,33 +1,33 @@
 # Puppet file for installing nginx on the webserver
 
 class nginx {
-	package { 'nginx':
-	  ensure => installed,
-	}
+  package { 'nginx':
+    ensure => installed,
+  }
 
-	service { 'nginx':
-	  ensure  => running,
-	  enable  => true,
-	  require => Package['nginx']
-	}
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx']
+  }
 
-	file { '/var/www/html/index.html':
-	  ensure  => 'present',
-	  content => 'Hello World!',
-	}
+  file { '/var/www/html/index.html':
+    ensure  => 'present',
+    content => 'Hello World!',
+  }
 
-	exec {'start_server':
-	  command => 'sudo service nginx start',
-	}
+  exec {'start_server':
+    command => 'sudo service nginx start',
+  }
 
-	file { '/var/www/html/redirect.html':
-	  ensure  => 'present',
-	  content => 'Moved Permanently',
-	}
+  file { '/var/www/html/redirect.html':
+    ensure  => 'present',
+    content => 'Moved Permanently',
+  }
 
-	exec { 'modify_config':
-	  command => "sed -i '/server_name _;/a \tlocation /redirect_me {\n\troot var/www/html/;\n}\n' /etc/nginx/sites-available/default",
-	  path    => ['/bin', '/usr/bin'],
-	}
+  exec { 'modify_config':
+    command => "sed -i '/server_name _;/a \tlocation /redirect_me {\n\troot var/www/html/;\n}\n' /etc/nginx/sites-available/default",
+    path    => ['/bin', '/usr/bin'],
+  }
 }
 include nginx
