70ce1527903facdd1a44f89b1263fd0ce51c84f3
BENONI ESCKINDER <besckinder@gmail.com>
2024-01-16 23:55:26 +0300

    Redirects are forbidden

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subr_name):
     ''' Gets the number of subscribers of a subreddit '''
     response = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subr_name),
-                            headers={"User-Agent": "GetSubscriberCount"})
+                            headers={"User-Agent": "GetSubscriberCount"},
+                            allow_redirects=False)
 
     data = response.json().get('data')
     if not data:
