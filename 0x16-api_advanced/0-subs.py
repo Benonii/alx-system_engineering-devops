26f8708f95f06a1d9affcb643a707a55f5af8e16
BENONI ESCKINDER <you@example.com>
2024-01-17 00:33:50 +0300

    Rearranging the order of imports

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 ''' This module contains the function number_of_subscribers '''
-import requests
 import json
-import sys
+import requests
 
 
 def number_of_subscribers(subreddit):
