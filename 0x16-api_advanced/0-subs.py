09bc55385cc742058df215259c23370041bde468
BENONI ESCKINDER <besckinder@gmail.com>
2024-01-16 23:52:44 +0300

    Adding doumentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+''' This module contains the function number_of_subscribers '''
 import requests
 import json
 import sys
 
 
 def number_of_subscribers(subr_name):
+    ''' Gets the number of subscribers of a subreddit '''
     response = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subr_name),
                             headers={"User-Agent": "GetSubscriberCount"})
