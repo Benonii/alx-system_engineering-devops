72e64f2bf6a2f03073d761d519db79aa52e609f8
BENONI ESCKINDER <besckinder@gmail.com>
2024-01-17 19:53:06 +0300

    Mission accomplished... I hope

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# This manifest fixes the root directory of the Apache 2 server and restarts it
+
+exec {'fix_root_dir_path':
+  command => "sed -i '12s/$/\/wp-content' /etc/apache2/sites-available/000-default.conf",
+  path    => 'usr/bin/:bin',
+}
+
+exec {'restart_apache2_server':
+  command => 'sudo service apache2 restart',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+We will be debugging an wordpress web stack. And we will be finding the error using strace
