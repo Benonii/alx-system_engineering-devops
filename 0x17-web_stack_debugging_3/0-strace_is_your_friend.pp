930bf6c4225e432408815a2329375fbfa42a169a
BENONI ESCKINDER <besckinder@gmail.com>
2024-01-17 20:50:08 +0300

    sed specifics

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,13 @@
 # This manifest fixes the root directory of the Apache 2 server and restarts it
 
+$conf_file = '/etc/apache2/sites-available/000-default.conf'
+
 exec {'fix_root_dir_path':
-  command => "sed -i '12s/$/\/wp-content' /etc/apache2/sites-available/000-default.conf",
-  path    => 'usr/bin/:bin',
+  command => "sed -i '12 s/$/wp-content/ ${conf_file}",
+  path    => ['/usr/bin/', '/bin/'],
 }
 
 exec {'restart_apache2_server':
   command => 'sudo service apache2 restart',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
