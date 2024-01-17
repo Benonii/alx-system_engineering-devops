f28e733b1e498c8617a68d320128578c354929cb
BENONI ESCKINDER <you@example.com>
2024-01-17 21:15:54 +0300

    For the love of God please work

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,9 @@
-# This manifest fixes the root directory of the Apache 2 server and restarts it
+#This manifest fixes the root directory of the Apache 2 server and restarts it
 
 $conf_file = '/etc/apache2/sites-available/000-default.conf'
 
 exec {'fix_root_dir_path':
-  command => "sed -i '12 s/$/wp-content/ ${conf_file}",
+  command => "sed -i '12 s/$/\/wp-content/' ${conf_file}",
   path    => ['/usr/bin/', '/bin/'],
 }
 
