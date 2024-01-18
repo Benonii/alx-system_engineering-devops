2733432c93266c24998b200700181b039b4ffbbd
BENONI ESCKINDER <besckinder@gmail.com>
2024-01-18 03:08:57 +0300

    Just making sure

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,24 +5,26 @@ import json
 import requests
 
 
-def recurse(subreddit, hot_list=[], page=None):
+def recurse(subreddit, hot_list=[], after=None):
     ''' Gets the all the hot articles in a subreddit '''
-    if page is None:
+    if after is None:
         url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
     else:
         url = "https://www.reddit.com/r/{}/hot.json?limit=100&after={}"\
-              .format(subreddit, page)
+              .format(subreddit, after)
 
     response = requests.get(url, headers={"User-Agent": "GetHotArticles/2.0"},
                             allow_redirects=False).json()
 
     data = response.get('data')
     if not data:
-        return hot_list
+        return None
     else:
-        for child in data['children']:
-            hot_list.append(child['data']['title'])
+        if 'children' in data:
+            for child in data['children']:
+                hot_list.append(child['data']['title'])
 
-        page = data['after']
+            after = data['after']
 
-        return recurse(subreddit, hot_list, page)
+            return recurse(subreddit, hot_list, after)
+        return hot_list
