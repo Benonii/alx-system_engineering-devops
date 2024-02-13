1d2b88c81c95a60d964cb563be577d8f759b82bd
BENONI ESCKINDER <you@example.com>
2024-02-13 22:35:47 +0300

    Commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+''' This module contains the function top_ten '''
+import json
+import requests
+
+
+def top_ten(subreddit):
+    ''' Gets the top ten topics of subscribers of a subreddit '''
+    response = requests.get("https://www.reddit.com/r/{}/top.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "GetHotTopics"},
+                            allow_redirects=False).json()
+
+    data = response.get('data')
+    if not data:
+        print(None)
+    else:
+        titles = []
+        for child in data['children']:
+            print(child['data']['title'])
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+This is a more advanced version on working with APIs We will be working with the Reddit API
